majacQ/uemacs

commit b8e555ac8d9093e7e5bc29aade6533ecd5086b3e

Get rid of odd 'throw repeated commands away during type-ahead' logic
This was the logic that messed up ^D auto-repeat, and while I fixed the
thing that triggered it, I think the whole logic is very wrong.

I suspect it comes from slow-updating terminals where it's hard to tell
where you are with auto-repeating keyboards because the repeat rate is
higher than the re-draw speed.

Anyway, I replaced it with logic that just says "don't bother updating
the screen if there's more data coming".  Which makes a big difference
when pasting in large amounts of text and is actually relevant these
days.

Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

main.c
@@ -116,7 +116,6 @@ int main(int argc, char **argv)
 	int saveflag;				/* temp store for lastflag */
 	int errflag;				/* C error processing? */
 	char bname[NBUFN];			/* buffer name of file to read */
-	int newc;
 
 	const char *aff_path = "/usr/share/hunspell/en_US.aff";
 	const char *dic_path = "/usr/share/hunspell/en_US.dic";
@@ -276,23 +275,10 @@ int main(int argc, char **argv)
 	execute(META | SPEC | 'C', FALSE, 1);
 	lastflag = saveflag;
 
-	if (typahead()) {
-		newc = getcmd();
+	if (!typahead())
 		update(FALSE);
-		do {
-			fn_t execfunc;
+	c = getcmd();
 
-			if (c == newc && (execfunc = getbind(c)) != NULL
-			    && execfunc != insert_newline && execfunc != insert_tab)
-				newc = getcmd();
-			else
-				break;
-		} while (typahead());
-		c = newc;
-	} else {
-		update(FALSE);
-		c = getcmd();
-	}
 	/* if there is something on the command line, clear it */
 	if (mpresf != FALSE) {
 		mlerase();

commit 8bd72de2506fcf218f3a235625dc708b0a70ce73

Always call update() if the buffer is marked for refresh
The type-ahead logic skipped calling update() when there were more keys
pending, which was really efficient for cut-and-paste.

However, it turns out that while it's efficient, it's a bit annoying, in
that now cut-and-paste acts rather differently from actually typing in
the characters normally.

In particular, the cursor location ends up affecting how the screen
contents are scrolled around, and without calling update() you end up
with newly pasted text possibly just left off-screen.  It was inserted
into the test buffers, but the cursor stays at the top of the screen so
you don't even realize.

So skip the efficiency for now.

Fixes: b8e555a ("Get rid of odd 'throw repeated commands away during type-ahead' logic")
Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

main.c
@@ -275,7 +275,7 @@ int main(int argc, char **argv)
 	execute(META | SPEC | 'C', FALSE, 1);
 	lastflag = saveflag;
 
-	if (!typahead())
+	if (curwp->w_flag || !typahead())
 		update(FALSE);
 	c = getcmd();
 